7982e7a66658549c184344ed6956cea335c9b086
Siza <sizasl@gmail.com>
2019-10-24 21:42:23 +1300

    Add posts with dummy data

---
+++ django_project/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+posts = [
+   {
+      'author': 'SizaSL',
+      'title': 'Blog Post 1',
+      'content': 'First post content',
+      'date_posted': 'Oct 24, 2019'
+   },
+   {
+      'author': 'SizaSL',
+      'title': 'Blog Post 1',
+      'content': 'Second post content',
+      'date_posted': 'Oct 25, 2019'
+   }
+]
+
+def home(request):
+   context = {
+      'posts': posts
+   }
+   return render(request, 'blog/home.html', context)
+
+def about(request):
+   return render(request, 'blog/about.html')
